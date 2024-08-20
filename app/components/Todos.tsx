c2c7b570b0b8452795314871d9bd9fe127301a55
Khadar abdi hassan <112935673+Khadar-abdi@users.noreply.github.com>
2024-08-20 11:16:09 +0300

    first commit

--- app/components/Todos.tsx
+++ app/components/Todos.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import TodoList from './todoList'
+import Link from 'next/link';
+import {sort} from 'fast-sort'
+import { TiArrowSortedUp } from "react-icons/ti";
 
 
 export interface todoItemsprops {
@@ -8,21 +11,27 @@ export interface todoItemsprops {
   isDone: boolean
 
 }
+interface props {
+  sortOrder: string;
+}
 
 
-const Todos = async() => {
+const Todos = async({sortOrder}:props) => {
 
   const res = await fetch("http://localhost:3000/api/todo", { cache:'no-cache'})
   const data : todoItemsprops[] = await res.json()
+  const sortedOrder = sort(data).asc(sortOrder=== 'title'? data=> data.title : data => data.isDone )
   
   return (
     <div className='flex flex-col  w-2/5 gap-4 shadow-md shadow-slate-700 p-[3rem]'>
         <div className='flex flex-row gap-[14rem] px-4 text-base  font-mono'>
-        <h3  >Name</h3>
-               <h3>Action</h3>
+        <h3 >
+        <Link href="/?sortOrder=title" className='flex flex-row items-center'> name <TiArrowSortedUp/></Link>
+        </h3>
+        <Link href="/?sortOrder=done" className='flex flex-row items-center'> Action <TiArrowSortedUp/></Link>
                </div>
                <div className='flex flex-col justify-between '>
-    {data.map((todo)=>(
+    {sortedOrder.map((todo)=>(
  
       <TodoList key={todo._id}  todo ={ todo  } />
     ))}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import TodoForm from "./components/todoForm";
 import Todos from "./components/Todos";
 
-export default function Home() {
+
+interface Props{
+  searchParams: {sortOrder: string}
+}
+
+export default function Home({searchParams:{sortOrder}}: Props) {
   return (
     <main className="flex min-h-screen flex-col items-center  gap-5 p-24">
        <TodoForm/>
-       <Todos/>
+       <Todos sortOrder={sortOrder}/>
     </main>
   );
 }
